9576bfc964775e821b6c2b34fd50c82c0d2a3086
foued <foued@gmail.com>
2022-02-19 23:49:35 +0100

    pagination ok

--- core/settings.py
+++ core/settings.py
@@ -117,6 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK  = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
     'NON_FIELD_ERRORS_KEY' : 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
